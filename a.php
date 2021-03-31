e3b6aa7a3607b841d0d9eaf0237c7fb2cd49c493
pratiksu <pratiksuryawanshi003@gmail.com>
2021-03-31 16:11:11 +0530

    sample eddit test

--- a.php
+++ a.php
@@ -2,8 +2,10 @@
 <html>
 <body>
 <form>
-firstname <input type="text" name="firstname" ><br><br>
-lastname <input type="text" name="lastname" >
+first name <input type="text" name="firstname" ><br><br>
+last name <input type="text" name="lastname" >
+
+
 <input type ="submit" name="submit" class="submit" >
 </form>
 </body>
